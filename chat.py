4f458dc2593e9fc9384eaa3082361ffff09d6fdd
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-04-12 23:44:08 +0800

    add check name

--- chat.py
+++ chat.py
@@ -6,19 +6,22 @@ def read_file(filename):
 			lines.append(line.strip())
 	return lines
 #將對話紀錄轉換所想要的形式
-def convert(lines):
+def convert(lines, name1, name2):
 	new = []
 	person = None
 	for line in lines:
-		if line == 'Allen':
-			person ='Allen'
+		if line == name1:
+			person = name1
 			continue
-		elif line == 'Tom':
-			person = 'Tom'
+		elif line == name2:
+			person = name2
 			continue
 		if person:
 			new.append(person + ':' + line)
-	return new
+		else:
+			print('你輸入的人名未在此對話紀錄')
+			return 0
+			break
 #寫入新的對話紀錄
 def writ_file(filename, output):
 	with open (filename, 'w', encoding = 'utf-8') as file:
@@ -27,9 +30,11 @@ def writ_file(filename, output):
 #主程式執行
 def main():
 	user_input = input('請輸入想讀取的對話紀錄: ')
+	user_name1 = input('請輸入對話紀錄內人名1: ')
+	user_name2 = input('請輸入對話紀錄內人名2: ')
 	lines = read_file(user_input)
-	lines = convert(lines)
-	writ_file('output.txt', lines)
-
-
+	lines = convert(lines, user_name1, user_name2)
+	if lines != 0:
+		writ_file('output.txt', lines)
+		
 main()
